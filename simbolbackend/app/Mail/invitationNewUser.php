69849f954da0ca86b7da89a886939b3b902f472b
robert <martiro12@gmail.com>
2019-03-13 23:52:33 -0300

    Set subject email new invitation

--- simbolbackend/app/Mail/invitationNewUser.php
+++ simbolbackend/app/Mail/invitationNewUser.php
@@ -28,6 +28,6 @@ class invitationNewUser extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.invitationNewUser');
+        return $this->markdown('emails.invitationNewUser')->subject('Invitación Simbol Club');
     }
 }
